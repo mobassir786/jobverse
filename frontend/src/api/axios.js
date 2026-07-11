12f7266468040f66ebd50e08fc3cd118d617f51d
Mobassir raza <mobassirraza83@gmail.com>
2026-07-11 23:35:08 +0530

    Change API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://jobverse-nhfv.onrender.com/api",
 });
 
 // Automatically attach the logged-in user's token to every request
